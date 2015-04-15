988349ed0be07db1bb40682bf8f7ea811c461c74
jhuang <jhuang@demystdata.com>
2015-04-15 14:36:39 +0800

    enable checksum, need to fix casting bug

--- rdt-part3.h
+++ rdt-part3.h
@@ -419,7 +419,7 @@ int rdt_recv(int fd, char * msg, int length){
 		ack[3] = '0';
 		// printf("unsigned short ckm: %hu\n", ckm);
 		// printf("checksum_in_char: %u, %u\n", checksum_in_char[0], checksum_in_char[1]);
-		if (false){
+		if (checksum_in_char[0]!='0' || checksum_in_char[1]!='0'){
 				//corrupted
 				printf("message corrupted\n");
 				//resend last ACK
